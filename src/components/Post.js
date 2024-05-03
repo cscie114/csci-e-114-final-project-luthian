5a03089455dd379a6d2125101a1771f0e656a4be
DDA <github@dantonio.net>
2024-05-02 22:18:37 -0400

    Test Posts component

--- src/components/Post.js
+++ src/components/Post.js
@@ -4,7 +4,7 @@ import * as postStyles from './post.module.css';
 
 function findMatches(list, allElements, key = 'id') {
   if (!list || !allElements) return ['None'];
-  return list.map((item) => allElements.find((el) => el[key] === item).name);
+  return list.map(item => allElements.find(el => el[key] === item).name);
 }
 
 function Post({ post, tags, categories }) {
@@ -12,35 +12,42 @@ function Post({ post, tags, categories }) {
     <section>
       <div className="text-center">
         <p className={postStyles.largeLineHeight}>
-          <span className="text-xl font-semibold" dangerouslySetInnerHTML={{ __html: post.title.rendered }} />
+          <span
+            data-testid="title"
+            className="text-xl font-semibold"
+            dangerouslySetInnerHTML={{ __html: post.title.rendered }}
+          />
         </p>
       </div>
       <ul className={postStyles.tileGrid}>
-        {post.images.map((image) => (
+        {post.images.map(image => (
           <li className={postStyles.postTile} key={image.url}>
             <figure>
-              <img className={postStyles.postImage} src={image.url} alt={image.altText} />
+              <img data-testid="image" className={postStyles.postImage} src={image.url} alt={image.altText} />
               <figcaption>
-                <span dangerouslySetInnerHTML={{ __html: image.caption }} />
+                <span data-testid="caption" dangerouslySetInnerHTML={{ __html: image.caption }} />
               </figcaption>
             </figure>
           </li>
         ))}
       </ul>
       <div className="text-left ml-8">
-        <span className={postStyles.desc} dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }} />
-        {' '}
+        <span
+          data-testid="excerpt"
+          className={postStyles.desc}
+          dangerouslySetInnerHTML={{ __html: post.excerpt.rendered }}
+        />{' '}
       </div>
       <div className="text-left ml-8 text-sm text-gray-500">
-        <p>
+        <p data-testid="tags">
           Ingredients:&nbsp;
           {findMatches(post.tags, tags, 'tagId').join(', ')}
         </p>
-        <p>
+        <p data-testid="categories">
           Type:&nbsp;
           {findMatches(post.categories, categories, 'categoryId').join(', ')}
         </p>
-        <p>
+        <p data-testid="date">
           Date:&nbsp;
           {post.date}
         </p>
@@ -51,8 +58,8 @@ function Post({ post, tags, categories }) {
 
 Post.propTypes = {
   post: PropTypes.object,
-  tags: PropTypes.object,
-  categories: PropTypes.object,
+  tags: PropTypes.array,
+  categories: PropTypes.array,
 };
 
 export default Post;
