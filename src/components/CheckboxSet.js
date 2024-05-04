dbe95173d748e7d1ee8531e1cb0ea02c895c9f80
DDA <github@dantonio.net>
2024-05-04 14:47:57 -0400

    Add new CheckboxSet components for filtering posts
    - Right now, it is _or_ between filters in a set and _and_ between filters sets.

---
+++ src/components/CheckboxSet.js
@@ -0,0 +1,55 @@
+import * as React from 'react';
+import PropTypes from 'prop-types';
+
+function CheckboxSet({ checkboxes, callback, title }) {
+  const [checkboxValues, setCheckboxValues] = React.useState(new Array(checkboxes.length).fill(false));
+
+  React.useCallback(() => { callback(checkboxValues); }, [callback, checkboxValues]);
+
+  React.useEffect(() => {
+    const newArray = checkboxes.filter((value, index) => checkboxValues[index]).map(item => item.value);
+    callback(newArray);
+  }, [checkboxValues]);
+
+  return (
+    <div className="m-3">
+      <span className="flex flex-row place-content-between">
+        <h3 className="text-center">{title}</h3>
+        <button
+          type="button"
+          className="text-blue-500 underline cursor-pointer block text-xs"
+          onClick={() => setCheckboxValues(new Array(checkboxes.length).fill(false))}
+        >
+          Clear All
+        </button>
+      </span>
+      {checkboxes.map((checkbox, index) => (
+        <label key={checkbox.value} className="flex flex-row items-start mt-3">
+          <input
+            type="checkbox"
+            name={checkbox.label}
+            checked={checkboxValues[index]}
+            onChange={e => {
+              const newCheckboxValues = [...checkboxValues];
+              newCheckboxValues[index] = e.target.checked;
+              setCheckboxValues(newCheckboxValues);
+            }}
+            className="mr-2 mt-1"
+          />
+          <span>
+            <div className="leading-tight">{checkbox.label}</div>
+            <div className="text-xs text-gray-500">{checkbox.description}</div>
+          </span>
+        </label>
+      ))}
+    </div>
+  );
+}
+
+export default CheckboxSet;
+
+CheckboxSet.propTypes = {
+  checkboxes: PropTypes.array,
+  callback: PropTypes.func,
+  title: PropTypes.string,
+};
