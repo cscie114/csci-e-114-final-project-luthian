5942177458ab0a3fb22833926663b90bd50eac8a
DDA <github@dantonio.net>
2024-05-05 19:50:50 -0400

    Fix linting errors

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,15 +10,15 @@ function IndexPage() {
       <p>
         This is my submission for the Final Project in{' '}
         <span className="font-semibold">CSCI E-114 Web Application Development with Jamstack</span>. This project is
-        very free form, so I decided to create a simple Gatsby site that showcases a few of the appetizer trays I've
+        very free form, so I decided to create a simple Gatsby site that showcases a few of the appetizer trays I&aposve
         made for various occasions. Gatsby pulls data from a WordPress site which is being used as a headless CMS. The
         site is hosted on Netlify and the WordPress site is hosted on a nano droplet on Digital Ocean. The site is
         styled with Tailwind CSS and uses the Gatsby Image plugins to optimize images. To make things a bit more
-        interesting, I've added filtering; the user can filter the appetizer trays by ingredients and the type of event
+        interesting, I&aposve added filtering; the user can filter the appetizer trays by ingredients and the type of event
         for which they were made.
       </p>
       <p className='mt-3'>
-        Note that this project doesn't use any special Gatsby plugins related to WordPress. The data is fetched via the
+        Note that this project doesn&post use any special Gatsby plugins related to WordPress. The data is fetched via the
         stock WordPress REST API and the images are processed on the Gatsby side.
       </p>
       <StaticImage
